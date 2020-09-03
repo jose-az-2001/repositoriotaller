055d567eac4b3c9599e4c138bed349aea9d51165
ppuacgarcia <davidpuacgarcia2@gmail..com>
2020-09-03 09:30:13 -0600

    menu
    este es el menu para la calculadora

--- source.cpp
+++ source.cpp
@@ -3,7 +3,38 @@
 using namespace std;
 
 int main(){
+    int seleccion=0;
+    do {
+        do {
+           cout << "ingrese uno de los valores 1.suma 2.Resta 3.Multiplicacion 4.Division 5.elevacion 6.raiz 7.salir" << endl;
+           cin >> seleccion;
+        } while (seleccion > 7 && seleccion < 1);
+        if (seleccion == 1)
+        {
 
+        }
+        else if (seleccion == 2)
+        {
+            
+        }
+        else if (seleccion == 3)
+        {
+            
+        }
+        else if (seleccion == 4)
+        {
 
+        }
+        else if (seleccion == 5)
+        {
+
+        }
+        else if (seleccion == 6)
+        {
+
+        }
+    } while(seleccion==7);
+       
+      
     return 0;
 }
