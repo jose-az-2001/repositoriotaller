563dd5cb551dbc958e24655da5355b5dd6e73e74
ppuacgarcia <davidpuacgarcia2@gmail..com>
2020-09-03 09:39:26 -0600

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <math.h>
+
 
 using namespace std;
-
+float Multipliacion(int n1, int n2) {
+    int resultado = 0;
+    resultado = n1 * n2;
+    return resultado;
+}
+float Elevacion(int n1,int n2){
+    int resultado;
+    resultado = pow(n1, n2);
+    return resultado;
+}
 int main(){
     int seleccion=0;
     do {
@@ -19,10 +30,22 @@ int main(){
         }
         else if (seleccion == 3)
         {
-            
+            int n1, n2;
+            cout << "ingrese el primer valor" << endl;
+            cin >> n1;
+            cout << "ingrese el segundo valor" << endl;
+            cin >> n2;
+            cout <<"el valor de la multipliacion entre "<<n1<<" y "<<n2<<" es "<< Multipliacion(n1, n2)<<endl;
         }
         else if (seleccion == 4)
         {
+            int n1, n2;
+            cout << "ingrese el  valor" << endl;
+            cin >> n1;
+            cout << "ingrese el valor al que sera elevado" <<n1<< endl;
+            cin >> n2;
+            cout << n1 << " elevado a " << n2 << "es igual a" <<Elevacion(n1,n2)<< endl;
+
 
         }
         else if (seleccion == 5)
